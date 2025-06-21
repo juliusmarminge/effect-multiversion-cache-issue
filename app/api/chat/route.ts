a8aad62c8fa83e6584a8ce45fa803652a7fc15c1
juliusmarminge <jmarminge@gmail.com>
2025-06-21 13:31:51 +0200

    last idea?

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,34 +1,49 @@
 import { MockLanguageModelV1 } from "ai/test";
 import { streamText, simulateReadableStream, createDataStream } from "ai";
+import { UTApi } from "uploadthing/server";
+
+const utapi = new UTApi({
+  token:
+    "eyJhcHBJZCI6ImFwcC0xIiwiYXBpS2V5Ijoic2tfZm9vIiwicmVnaW9ucyI6WyJmcmExIl19",
+});
 
 const generateChatStream = async () => {
   console.log("Generating chat stream");
 
   const dataStream = createDataStream({
     execute: (dataStream) => {
-      const stream = streamText({
-        model: new MockLanguageModelV1({
-          doStream: async () => ({
-            stream: simulateReadableStream({
-              chunks: [
-                { type: "text-delta", textDelta: "Hello" },
-                { type: "text-delta", textDelta: ", " },
-                { type: "text-delta", textDelta: `world!` },
-                {
-                  type: "finish",
-                  finishReason: "stop",
-                  logprobs: undefined,
-                  usage: { completionTokens: 10, promptTokens: 3 },
-                },
-              ],
-            }),
-            rawCall: { rawPrompt: null, rawSettings: {} },
-          }),
-        }),
-        prompt: "Hello, test!",
-      });
+      if (Math.random() * 100 > 0.01) {
+        // do image gen
+        // ...
 
-      stream.mergeIntoDataStream(dataStream);
+        // upload image
+        void utapi.uploadFiles(new File([], "test.txt"));
+      } else {
+        // do text gen
+        const stream = streamText({
+          model: new MockLanguageModelV1({
+            doStream: async () => ({
+              stream: simulateReadableStream({
+                chunks: [
+                  { type: "text-delta", textDelta: "Hello" },
+                  { type: "text-delta", textDelta: ", " },
+                  { type: "text-delta", textDelta: `world!` },
+                  {
+                    type: "finish",
+                    finishReason: "stop",
+                    logprobs: undefined,
+                    usage: { completionTokens: 10, promptTokens: 3 },
+                  },
+                ],
+              }),
+              rawCall: { rawPrompt: null, rawSettings: {} },
+            }),
+          }),
+          prompt: "Hello, test!",
+        });
+
+        stream.mergeIntoDataStream(dataStream);
+      }
     },
   });
 
