2d7c435bd1181b599b638c727fe4df81e91b5f46
juliusmarminge <jmarminge@gmail.com>
2025-06-21 12:13:35 +0200

    catch

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 export const dynamic = "force-dynamic";
 
 const resolveUrl = (path: string) => {
-  const vercel = process.env.VERCEL_ENV === "production"
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : process.env.VERCEL_ENV === "preview"
-    ? `https://${process.env.VERCEL_PROJECT_PREVIEW_URL}`
-    : undefined;
+  const vercel =
+    process.env.VERCEL_ENV === "production"
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+      : process.env.VERCEL_ENV === "preview"
+      ? `https://${process.env.VERCEL_PROJECT_PREVIEW_URL}`
+      : undefined;
 
   const base = vercel || "http://localhost:3000";
   return `${base}${path}`;
 };
 
 export default async function Home() {
-  const data = await (await fetch(resolveUrl("/api/uploadthing"))).json();
+  const data = await (await fetch(resolveUrl("/api/uploadthing")))
+    .json()
+    .catch((error) => {
+      console.error(error);
+      return null;
+    });
 
   return (
     <div>
